9ee52ef17ee7225b60b93859e12b5fe9d231d4ed
chati <lilylee9292@gmail.com>
2020-02-12 22:09:49 +0900

    lombok extra setting and test code for lombok

--- src/main/java/com/moimone/web/SimpleController.java
+++ src/main/java/com/moimone/web/SimpleController.java
@@ -1,6 +1,8 @@
 package com.moimone.web;
 
+import com.moimone.web.dto.SimpleResponse;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,6 +11,11 @@ public class SimpleController {
     @GetMapping("/helloWorld")
     public String Hello() {
         return "helloWorld";
+    }
+
+    @GetMapping("/simple/response")
+    public SimpleResponse helloResponse(@RequestParam("name") String name, @RequestParam("amount") int amount) {
+        return new SimpleResponse(name, amount);
     }
 
 }
